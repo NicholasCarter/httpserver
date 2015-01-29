56dd86095535a0eeec87a4e260f7bb8b8701aeb1
Nicholas Carter <carterni@mail.gvsu.edu>
2015-01-29 14:32:49 -0500

    Implemented existing methods within HttpDocroot.java

--- HttpResponse.java
+++ HttpResponse.java
@@ -1,7 +1,16 @@
 class HttpResponse
 {
 	HttpRequest request;
-
+	int code;
+	/*
+	 * 1xx not needed
+	 * 2xx request correct, recieved, and understood
+	 * 304 file not changed since
+	 * 4xx incorrect request
+	 * 5xx correct request, but failure on server's part.
+	 */
+	String reason;
+	
 	public HttpResponse(HttpRequest request) {
 		this.request = request;
 	}
